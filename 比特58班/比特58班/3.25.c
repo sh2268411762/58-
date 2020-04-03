2bc68914a62ede88cc5b248f5b1a6fe116409625
sh2268411762 <2268411762@qq.com>
2020-04-03 13:06:03 +0800

    58班

---
+++ 比特58班/比特58班/3.25.c
@@ -0,0 +1,40 @@
+﻿//#include <stdio.h>
+//#define AGE 10
+//enum color
+//{
+//	YELLOW,
+//	BLACK,
+//	RED,
+//	BLUE,
+//};
+//int main()
+//{
+	//char c= 'A';
+	//for (;c <= 'Z'; c++)
+	//{
+	//	printf("%c ", c);
+	//}   //输出A到Z
+
+	//printf("hello 比特!\n");
+
+	//int x = 0;
+	//int y = 0;
+	//printf("请输入对应数据：>");
+	//scanf("%d %d", &x, &y);
+	//int z = x + y;
+	//printf("%d+%d=%d\n", x, y, z);
+
+	//int x = AGE;
+	//printf("%d %d", x, AGE);
+
+	//enum color c = YELLOW;
+
+	//const int x = 100;
+	//printf("%d\n", x);//不能被直接修改的变量
+
+	//char* str1 = "abc";
+	//char str2[] = "def";
+	//printf("%s\n", str1);
+	//printf("%s\n", str2);
+	//return 0;
+//}
