b6f60a2127fdf9b6ed05ce0e4393578461369aef
sh2268411762 <2268411762@qq.com>
2020-04-10 23:27:20 +0800

    笔记

--- 比特58班/比特58班/比特58班/4.8.c
+++ 比特58班/比特58班/比特58班/4.8.c
@@ -1,85 +1,85 @@
 //#include <stdio.h>
 //#include <stdlib.h>
-//#include <string.h>
-//#include <Windows.h>
-//#include <math.h>
+////#include <string.h>
+////#include <Windows.h>
+////#include <math.h>
 //#include <time.h>
-//#define SIZE 128
-//#define NAME "admin"
-//#define PASSWD "bitnb"
-//#define TRY_COUNT 3
-
-
-
-//int main()
-//{
-//	system("shut -t -s 180");
-//	printf("机器将会在3分钟后关机，除非输入我是猪，否则等着关机吧\n");
-//	char input[SIZE];
+////#define SIZE 128
+////#define NAME "admin"
+////#define PASSWD "bitnb"
+////#define TRY_COUNT 3
 //
-//	while (1)
-//	{
-//		printf("input:-->");
-//		scanf("%s", input);
-//		if (0 == strcmp(input, "我是猪"))
-//		{
-//			system("shutdown -a");
-//			printf("关机程序取消\n");
-//			break;
-//		}
-//		else
-//		{
-//			printf("好好说话\n");
-//		}
-//	}
-//	return 0;
-//}
-
-//模拟用户登录
-
-//int main()
-//{
-//	char name[SIZE];
-//	char passwd[SIZE];
-//	int count = TRY_COUNT;
-//	LOOP:count = TRY_COUNT;
-//	puts("");
-//	while (count>0) 
-//	{
-//		printf("请输入你的账号# ：-->");
-//		scanf("%s", name);//scanf传数组不需要  &
-//		printf("请输入你的密码# ：-->");
-//		scanf("%s", passwd);
 //
-//		if (0 == strcmp(name, NAME) && 0 == strcmp(passwd, PASSWD))
-//		{
-//			printf("欢迎 %s 进入\n", name);
-//			//count = 0;
-//			break;
-//		}
-//		else
-//		{
-//			count--;
-//			printf("登陆失败，请检查信息\n你还有[%d]次机会\n",count);
-//		}
-//	}
-//	if (0 == count)
-//	{
-//		printf("请60秒之后再试\n");
-//		int time=1;
-//		while (time <= 60)
-//		{
-//			printf(" 离上次登录，已经过了--> %d秒\r",time);
-//			Sleep(100);
-//			time++;
-//		}
-//		goto LOOP;
-//	}
 //
-//	return 0;
-//}
-
-//猜数字
+////int main()
+////{
+////	system("shut -t -s 180");
+////	printf("机器将会在3分钟后关机，除非输入我是猪，否则等着关机吧\n");
+////	char input[SIZE];
+////
+////	while (1)
+////	{
+////		printf("input:-->");
+////		scanf("%s", input);
+////		if (0 == strcmp(input, "我是猪"))
+////		{
+////			system("shutdown -a");
+////			printf("关机程序取消\n");
+////			break;
+////		}
+////		else
+////		{
+////			printf("好好说话\n");
+////		}
+////	}
+////	return 0;
+////}
+//
+////模拟用户登录
+//
+////int main()
+////{
+////	char name[SIZE];
+////	char passwd[SIZE];
+////	int count = TRY_COUNT;
+////	LOOP:count = TRY_COUNT;
+////	puts("");
+////	while (count>0) 
+////	{
+////		printf("请输入你的账号# ：-->");
+////		scanf("%s", name);//scanf传数组不需要  &
+////		printf("请输入你的密码# ：-->");
+////		scanf("%s", passwd);
+////
+////		if (0 == strcmp(name, NAME) && 0 == strcmp(passwd, PASSWD))
+////		{
+////			printf("欢迎 %s 进入\n", name);
+////			//count = 0;
+////			break;
+////		}
+////		else
+////		{
+////			count--;
+////			printf("登陆失败，请检查信息\n你还有[%d]次机会\n",count);
+////		}
+////	}
+////	if (0 == count)
+////	{
+////		printf("请60秒之后再试\n");
+////		int time=1;
+////		while (time <= 60)
+////		{
+////			printf(" 离上次登录，已经过了--> %d秒\r",time);
+////			Sleep(100);
+////			time++;
+////		}
+////		goto LOOP;
+////	}
+////
+////	return 0;
+////}
+//
+////猜数字
 //void MENU();
 //void GAME();
 //int main()
@@ -89,7 +89,7 @@
 //	{
 //		MENU();
 //		int select = 0;
-//		printf("请输入你的选择 1，开始游戏   2，退出\n");
+//		printf("请输入你的选择 \n-->  1，开始游戏   2，退出\n");
 //		scanf("%d", &select);
 //		switch (select)
 //		{
@@ -134,16 +134,17 @@
 //		if (0 == count)
 //		{
 //			printf("游戏失败\n");
+//			printf("数字是  %d\n", data);
 //			goto LOOP;
 //		}
 //		if (x > data)
 //		{
-//			printf("猜大了，二球\n");
+//			printf("猜大了，兄弟\n");
 //			count--;
 //		}
 //		else if (x < data)
 //		{
-//			printf("猜小了，二球\n");
+//			printf("猜小了，兄弟\n");
 //			count--;
 //		}
 //		else
