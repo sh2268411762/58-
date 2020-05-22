ce10e66987bbcf1e231b6176e2bd26b3cc8aa762
sh2268411762 <2268411762@qq.com>
2020-05-22 22:21:42 +0800

    5.22代码

---
+++ 比特58班/比特58班/比特58班/5.22.c
@@ -0,0 +1,66 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//unsigned char i = 0;
+//int main()
+//{
+//	//int a = 10;
+//	//printf("0x：%p", a);
+//	//0x00 00 00 0A
+//	//char b = (char)a;
+//	//if (0 == b)
+//	//{
+//	//	printf("大端\n");
+//	//}
+//	//if (10 == b)
+//	//{
+//	//	printf("小端\n");
+//	//}
+//	//int c = -10;
+//	//1111 1111 1111 1111 1111 1111 1111 0110
+//	//0xff ff ff f6
+//	//int c = 0xfffffff6;
+//	
+//
+//	//char a = -1;
+//	//signed char b = -1;
+//	//unsigned char c = -1;//符号位参与运算
+//	//printf("a = %d,b = %d,c = %d", a, b, c);//-1 -1 255
+//
+//
+//	//char a = -128;
+//	//printf("%u\n", a);//4294967168
+//
+//	//char a = 128;
+//	//printf("%u\n", a);//4294967168
+//	//printf("%d\n", a);
+//
+//
+//	//int i = -20;
+//	//unsigned int j = 10;
+//	//printf("%d\n", i + j);//-10
+//
+//
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	//9 8 7 ……  1 0 4294967295  …… 
+//
+//	//char a[1000];
+//	//int i;
+//	//for (i = 0; i < 1000; i++)
+//	//{
+//	//	a[i] = -1 - i;
+//	//}
+//	//printf("%d", strlen(a));//255
+//
+//
+//	//for (i = 0; i <= 255; i++)
+//	//{
+//	//	printf("hello world\n");
+//	//}//死循环
+//	return 0;
+//}
